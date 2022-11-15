6db8f22d615f2a934802dbbf2513797f914c1d20
P-manBrowm <80106172+P-manBrown@users.noreply.github.com>
2022-11-15 16:18:20 +0900

    improve: add devcontainer features
    add devcontainer features to delete Dockerfile for devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,24 @@
 set -eux
 
-# setup GitHub CLI
+# setting up 'Bash'
+cat <<-'EOF' >> $HOME/.bashrc
+	if [ "$SHLVL" = 2 ]; then
+	  script --flush ~/bashlog/script/`date "+%Y%m%d%H%M%S"`.log
+	fi
+	export PROMPT_COMMAND='history -a'
+	export PS4='+\e[32m[\t]\e[34m[${BASH_SOURCE[0]##*/}:${LINENO}]\e[m> '
+	export HISTFILE=~/bashlog/.bash_history
+EOF
+sudo chown -R $(whoami) $HOME/bashlog
+mkdir -p $HOME/bashlog/script
+touch $HOME/bashlog/.bash_history
+
+# setting up 'commit template'
+git config --local commit.template ./.github/commit/gitmessage.txt
+
+# setting up 'GitHub CLI'
 gh auth login --with-token < ./.devcontainer/secrets/github-token.txt
+gh config set editor "code -w"
 
 copy_and_ignore() {
 	source_file="$1"
@@ -17,9 +34,9 @@ copy_and_ignore() {
 	cp --update "$source_file" "$target_dir"
 }
 
-# setup VSCode
+# setting up 'VSCode'
 copy_and_ignore ./.devcontainer/vscode/launch.json ./.vscode
 
-# setup Lefthook
+# setting up 'Lefthook'
 copy_and_ignore ./.devcontainer/lefthook/lefthook-local.yml ./
 yarn lefthook install
