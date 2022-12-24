dac8621c0ad7265aba1643efef3ddcf7b138df6f
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-12-24 11:42:52 +0900

    improve: change to use existing config file when starting a project

--- setup/scripts/create-pj.sh
+++ setup/scripts/create-pj.sh
@@ -1,20 +1,27 @@
+#!/bin/bash
 set -eu
 
-if [ ! -e /.dockerenv ]; then
-	printf '\e[31m%s\n\e[m' 'ERROR: This file must be run inside the container.'
+err() {
+	printf '\e[31m%s\n\e[m' "ERROR: $*" >&2
+}
+
+if [[ ! -e /.dockerenv ]]; then
+	err 'This file must be run inside the container.'
 	exit 1
 fi
 
-# used at 'mv setting files' section
+PROJECT_NAME="$(grep 'COMPOSE_PROJECT_NAME' ./.env | cut -f 2 -d '=')"
 PACKAGE_MANAGER="$(grep '"packageManager"' ./package.json)"
 
 # create project
 echo 'Creating your project...'
-PROJECT_NAME="$(grep 'COMPOSE_PROJECT_NAME' ./.env | cut -f 2 -d '=')"
-yarn create next-app $PROJECT_NAME --typescript --no-eslint
-rm -rf ./$PROJECT_NAME/.git
-mv -f ./$PROJECT_NAME/* ./$PROJECT_NAME/.[^\.]* ./
-rmdir ./$PROJECT_NAME
+yarn create next-app "${PROJECT_NAME}" --typescript --no-eslint
+mv -f "./${PROJECT_NAME}"/{*,.[^\.]*} ./
+rmdir "./${PROJECT_NAME}"
+
+# adding file contents to the index
+echo 'Adding file contents to the index...'
+git add .
 
 # install packages
 echo 'Installing packages...'
@@ -51,40 +58,27 @@ echo 'Setting up your project...'
 mkdir ./src
 mv ./{pages,styles} ./src
 ## mv setting files
-SETTINGS_DIR='./setup/settings'
-mv -f $SETTINGS_DIR/settings.json ./.vscode/settings.json
-mv -f $SETTINGS_DIR/globals.css ./src/styles/globals.css
-mv -f $SETTINGS_DIR/tsconfig.json ./tsconfig.json
-sed -i -e "/.pnp/d; /# dependencies/r $SETTINGS_DIR/.gitignore" ./.gitignore
-npm_script_names=($(tac ./setup/settings/npm-scripts.txt | cut -f 1 -d ':'))
-for n in "${npm_script_names[@]}"
-do
-	npm_script="$(grep "$n" ./setup/settings/npm-scripts.txt)"
-	if grep -q "$n" ./package.json; then
-		sed -i "/$n/c \    $npm_script" ./package.json
+CONFIG_DIR='./setup/config'
+mv -f "${CONFIG_DIR}/globals.css" ./src/styles/globals.css
+mv -f "${CONFIG_DIR}/tsconfig.json" ./tsconfig.json
+sed -i -e "/.pnp/d; /# dependencies/r ${CONFIG_DIR}/.gitignore" ./.gitignore
+while read -r npm_script; do
+	npm_script_name="$(echo "${npm_script}" | cut -d ':' -f 1)"
+	if grep -q "${npm_script_name}" ./package.json; then
+		sed -i "/${npm_script_name}/c \    ${npm_script}" ./package.json
 	else
-		sed -i "/\"lint\"/a \    $npm_script" ./package.json
+		sed -i "/\"lint\"/a \    ${npm_script}" ./package.json
 	fi
-done
-sed -i -e "s/  }$/  },/" -e "/^}$/i \\$PACKAGE_MANAGER" ./package.json
-code ./.gitignore
-code ./package.json
-if [[ "$PROJECT_NAME" =~ 'frontend' ]]; then
-	sed -i \
-		'/^commit-msg:$/e < $SETTINGS_DIR/lefthook/protect-branch.txt' lefthook.yml
-	cat $SETTINGS_DIR/lefthook/cleanup-branches.txt >> lefthook.yml
+done < <(tac "${CONFIG_DIR}/npm-scripts.txt")
+sed -i -e "s/  }$/  },/" -e "/^}$/i \\${PACKAGE_MANAGER}" ./package.json
+printf '\x1b[1m%s\e[m\n' 'Check the contents of .gitignore and package.json'
+if [[ "${PROJECT_NAME}" == *'frontend'* ]]; then
 	sed -i 's/main/develop/' ./.github/dependabot.yml
-	cat $SETTINGS_DIR/dependabot-npm.txt >> ./.github/dependabot.yml
+else
+	sed -i '/protect-branch:$/,/fail_text:.*branch\."$/d' ./lefthook.yml
 fi
-rm -rf $SETTINGS_DIR
-rm -f ./.eslintrc.json
-# add ignorepaths
-cat <<-EOF >> ./.git/info/exclude
-	/.vscode/setting.json
-	/html_from_md
-	.DS_Store
-EOF
+rm -rf "${CONFIG_DIR}"
+
+rm ./setup/scripts/create-pj.sh
 
 echo 'Done!!'
-
-rm -rf ./setup
